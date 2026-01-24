4ed6eea2c37e6d2ab541bf7784a44b39ac5fb0d2
Rohan3280 <rahul01sept@gmail.com>
2026-01-24 18:24:12 +0530

    First commit

---
+++ Java/src/com/ok/javanew/datatypes/Test.java
@@ -0,0 +1,20 @@
+package com.ok.javanew.datatypes;
+
+public class Test {
+    public static void main(String[] args)
+    {
+        System.out.println(8.909);
+        boolean kk =false;
+        System.out.println(kk);
+        System.out.println((int)Character.MAX_VALUE);
+        System.out.println((char)10084);
+        int r =10;
+        float o=r;
+        char p =  String.valueOf(r).charAt(0);
+        System.out.println(p);
+        String str1 = "Hello";
+        String str2 = "Hello";
+        String str3 = new String   
+     }
+
+}
