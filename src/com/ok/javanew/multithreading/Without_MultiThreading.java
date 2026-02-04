00ecbdbeec03119b08441edac23a4ef17f647898
Rohan3280 <rahul01sept@gmail.com>
2026-02-05 00:00:00 +0530

    MultiThreading 2

--- src/com/ok/javanew/multithreading/Without_MultiThreading.java
+++ src/com/ok/javanew/multithreading/Without_MultiThreading.java
@@ -1,4 +1,27 @@
 package com.ok.javanew.multithreading;
 
+
 public class Without_MultiThreading {
+    public static void main(String[] args)
+    {
+        long startTime=System.currentTimeMillis();
+        long sum =0;
+        for(int i=0;i<=1000000000;i++)
+        {
+            sum+=1;
+        }
+        System.out.println("Sum: "+sum);
+
+        int count=0;
+        for(int i=0;i<=1000000000;i++)
+        {
+            if(i%10==7)
+            {
+                count++;
+            }
+        }
+        System.out.println("Count of numbers ending with 7 is :"+count);
+        System.out.println("Time taken: "+ (System.currentTimeMillis()-startTime)+" ms");
+    }
+
 }
